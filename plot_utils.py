e4daa44e09c74f14a206ee6fbed853ba697125a7
Michele Veronesi <miverone@cern.ch>
2020-03-21 14:07:54 +0100

    bho

--- plot_utils.py
+++ plot_utils.py
@@ -1,14 +1,18 @@
 import numpy as np
 import math
 import matplotlib
-matplotlib.rc('text', usetex=True)
 import matplotlib.pyplot as plt
 import matplotlib.lines as lines
 import mplhep as hep
 plt.style.use(hep.style.ATLAS)
-plt.rc('font',**{'family':'serif','serif':['Times']})
+# plt.rc('font',**{'family':'serif','serif':['Times']})
 # plt.rc('text', usetex=False)
 # plt.rc('text', usetex=True)
+# plt.style.use(['science','ieee','no-latex'])
+from matplotlib import rc, rcParams
+rc('text', usetex=True)
+rc('font',**{'family':'serif','serif':['Times']})
+rcParams['text.latex.preamble']=[r'\usepackage{amsmath}']
 
 import decay_rate_compact
 from decay_rate_compact import C_qf, S_qf, A_qf
